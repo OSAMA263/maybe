362f19c22c9a5a7f277c392643a3bc3519c86af3
osama263 <osamaelseify2@gmail.com>
2024-06-08 11:58:01 +0300

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <HelmetProvider>
         <ChakraProvider disableGlobalStyle>
           <App />
         </ChakraProvider>
       </HelmetProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
